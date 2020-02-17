25daf282dd9f137a63659627173ae79fb8669849
patelh0486 <patelh0486@uhcl.edu>
2020-02-17 15:44:59 -0600

    problem C

---
+++ Test1Practce/C_Sentinel-controlledLoop/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using static System.Console;
+
+namespace C_Sentinel_controlledLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // WriteLine("enter a number:");
+           // int num = Convert.ToInt32(ReadLine());
+            int prev = 10;
+            int total = 0;
+            int count = 0;
+            int largest = int.MinValue;
+
+            for(; ; )
+            {
+                WriteLine("Enter a number : ");
+                int num = Convert.ToInt32(ReadLine());
+                total += num;
+                count += 1;
+                if ( (prev < 10 && prev >=-9) && (num < 10 && num >= -9))
+                {
+                   
+                    break;
+                }
+                if (num > largest)
+                {
+                    largest = num;
+                }
+                prev = num; ;
+            }
+            WriteLine("largest number: " + largest);
+            double average = (double)total / count;
+            WriteLine("Average : " + average);
+        }
+    }
+}
