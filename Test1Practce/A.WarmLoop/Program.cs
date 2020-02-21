bf476b1a3f5c5f9ec3ad32679348cc7885913d4d
patelh0486 <patelh0486@uhcl.edu>
2020-02-21 14:12:57 -0600

    warm loop

---
+++ Test1Practce/A.WarmLoop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console;
+
+namespace A.WarmLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = -70;
+            //bool isOdd = (i % 2 == -1) || (i % 2 == 1);
+
+            while (i < 10)
+            {
+                bool isOdd = (i % 2 == -1) || (i % 2 == 1);
+                if (isOdd)
+                {
+                    Write($"{i}, ");
+                   
+                }
+                i++;
+            }
+        }
+    }
+}
