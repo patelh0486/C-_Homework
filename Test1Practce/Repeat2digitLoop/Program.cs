7e9efde43054e76dff5b7629615559e9ce8bbf7e
patelh0486 <patelh0486@uhcl.edu>
2020-02-21 13:54:46 -0600

    repeat 2 digit

---
+++ Test1Practce/Repeat2digitLoop/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+
+namespace Repeat2digitLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i<=9; i++)
+            {
+                for(int j = 0; j<=9; j++)
+                {
+                    for(int k = 0; k <= 9; k++)
+                    {
+                        if (((i == j) || (i==k)) || (j==k))
+                        {
+                            if (!((i == j) && (j == k)))
+                            {
+                                Write($"{i}{j}{k}, ");
+                            }
+                        }
+                    }
+                }
+
+            } 
+        }
+    }
+}
